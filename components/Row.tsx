a23e68cb7049ca060f16884e627613589cf81fe9
rudra pramanik <rudrahimangshoo2020@gmail.com>
2022-05-25 11:08:28 +0600

    1.52

--- components/Row.tsx
+++ components/Row.tsx
@@ -22,18 +22,11 @@ const Row = ({ title, movies }) => {
           className="scrollbar-hide flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2"
           //   ref={rowRef}
         >
-          {/* {movies.map(movie)=>(
-             <Thumbnail key={movie.id} movie={movie} />
-
-            )} */}
           {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
-        <ChevronRightIcon
-          className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
-          //   onClick={() => handleClick('right')}
-        />
+        <ChevronRightIcon className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
       </div>
     </div>
   )
